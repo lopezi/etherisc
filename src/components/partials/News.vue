2ef0dd09aafa370aceba15b47eba631425fc5a66
2n1u0 <388476+lopezi@users.noreply.github.com>
2023-07-10 15:24:32 +0100

    multiple style optimisation

--- src/components/partials/News.vue
+++ src/components/partials/News.vue
@@ -4,22 +4,34 @@
         <div class="relative h-64 overflow-hidden rounded-lg md:h-80">
             <!-- Item 1 -->
             <div class="hidden duration-5700 ease-in-out" data-carousel-item>
-                <span class="absolute text-black z-20 block w-32 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
-                <h2 class="text-3xl mt-12 tracking-tight absolute text-special-blue z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" >Etherisc teams up with Chainlink to deliver crop insurance in Kenya</h2>
-                <img src="../../assets/background-news.svg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="absolute text-white z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    <span class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
+                    <h2 class="text-3xl mt-12 tracking-tight " >Etherisc teams up with Chainlink to deliver crop insurance in Kenya</h2>
+                    <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 dark:focus:ring-yellow-900">Read article</button>
+
+                </div>
+                <img src="../../assets/news-source.jpg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-5700 ease-in-out" data-carousel-item>
-                <span class="absolute text-black z-20 block w-32 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
-                <h2 class="text-3xl mt-16 tracking-tight absolute text-special-blue z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" >Etherisc Generic Insurance Framework (GIF)<br> Goes Multi-Chain</h2>
-                <img src="../../assets/background-news.svg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="absolute text-white z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    <span class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
+                    <h2 class="text-3xl mt-16 tracking-tight" >Etherisc Generic Insurance Framework (GIF)<br> Goes Multi-Chain</h2>
+                    <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 dark:focus:ring-yellow-900">Read article</button>
+
+                </div>
+                <img src="https://uploads-ssl.webflow.com/6246b5e5953914c18aebfa5a/626bcd555c81c0391c88e2bf_Element%20162.svg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
 
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <span class="absolute text-black z-20 block w-32 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
-                <h2 class="text-3xl mt-12 tracking-tight absolute text-special-blue z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" >Expand Inclusive Insurance in Sri Lanka </h2>
-                <img src="../../assets/background-news.svg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="absolute text-white z-10 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    <span class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">News</span>
+                    <h2 class="text-3xl mt-12 tracking-tigh" >Expand Inclusive Insurance in Sri Lanka </h2>
+                    <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 dark:focus:ring-yellow-900">Read article</button>
+
+                </div>
+                <img src="../../assets/news-source-2.jpg" class="z-0 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
         </div>
     </div>
